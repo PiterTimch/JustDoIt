cd3bedc6b607ed1c482c363d06aa621926ed81ff
Petro <petrotymchuck@gmail.com>
2026-01-06 19:25:22 +0200

    Fix config

--- app/src/main/java/com/example/justdoit/config/Config.java
+++ app/src/main/java/com/example/justdoit/config/Config.java
@@ -1,8 +1,8 @@
 package com.example.justdoit.config;
 
 public class Config {
-//    public static final String BASE_URL = "http://192.168.0.102:5240/";
-    public static final String BASE_URL = "http://10.168.45.232:5240/";
+    public static final String BASE_URL = "http://192.168.0.104:5240/";
+//    public static final String BASE_URL = "http://localhost/:5240/";
 
 //    public static final String BASE_URL = "http://192.168.0.101:5240/";
 
